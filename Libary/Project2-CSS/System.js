56d61e1fd83fc431636daa9ef2aea84d635c9e55
Flare133 <249882990+Flamemaster921@users.noreply.github.com>
2025-12-23 01:35:12 -0500

    design
    working on css

--- Libary/Project2-CSS/System.js
+++ Libary/Project2-CSS/System.js
@@ -3,10 +3,10 @@ const textarea = document.getElementById("usernameBox");
 const button = document.getElementById("submitButton");
 const nameDisplay = document.getElementById("nameDisplay");
 
-button.addEventListener("click", () => {
-    const textarea = document.getElementById("usernameBox");
-    if (textarea.value !== "") {   
-    nameDisplay.textContent = "Hello Welcome " + textarea.value;
-    textarea.value = "";
-    }
-});
+//button.addEventListener("click", () => {
+    //const textarea = document.getElementById("usernameBox");
+    //if (textarea.value !== "") {   
+    //nameDisplay.textContent = "Hello Welcome " + textarea.value;
+    //textarea.value = "";
+    //}
+//});
